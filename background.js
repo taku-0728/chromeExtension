811d9f970234299e095fb740bf0d6b0750ab40a5
taku-0728 <taku8270@gmail.com>
2022-12-04 21:05:43 +0900

    [mod] 一度同じページを開くと新規ページごとにモーダルが出るのを修正

--- background.js
+++ background.js
@@ -1,29 +1,28 @@
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  switch (msg.name) {
-      case "pagecheck":
-        chrome.tabs.query({currentWindow: true}, tabs => {
-          tabs.map((currentTab, i) => {
-            tabs.slice(i).map(tab => {
-              if (currentTab.id === tab.id) {
+  chrome.tabs.query({ active: true, currentWindow: true })
+    .then(activeTab => {
+      switch (msg.name) {
+        case "pagecheck":
+          chrome.tabs.query({currentWindow: true}).then(tabs => {
+            tabs.forEach(tab => {
+              if (activeTab[0].id === tab.id) {
                 return;
               }
-      
-              if (currentTab.url === tab.url) {
+              if (activeTab[0].url === tab.url) {
                 sendResponse({
-                  'currentTabID': currentTab.id,
+                  'currentTabID': activeTab[0].id,
                   'tabID': tab.id
                 });
                 return;
               }
             });
           });
-        });
-        break;
-      case 'removeTab':
-        chrome.tabs.remove(msg.tabID);
-        chrome.tabs.update(msg.currentTabID,{selected:true});
-        break;
-  }
-  
+          break;
+        case 'removeTab':
+          chrome.tabs.remove(msg.currentTabID);
+          chrome.tabs.update(msg.tabID,{selected:true});
+          break;
+    }
+    });
   return true;
 })
